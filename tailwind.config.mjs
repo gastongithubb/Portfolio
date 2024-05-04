b536e8ffcdd532f3a7255c94989ac9504d8052c3
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-05-04 05:52:38 -0300

    Se carga nuevo portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				"bebas-neuen": "bebas",
+			},
+			fontSize: {},
+			fontWeight: {},
+			lineHeight: {},
+			borderRadius: {},
+			colors: {
+				"neon-green": "#d5ff3f",
+				"black-blue": "#161b22",
+				"dark-blue": "#242938",
+			},
+			spacing: {},
+			width: {},
+			minWidth: {},
+			maxWidth: {},
+			height: {},
+			minHeight: {},
+			maxHeight: {},
+		  },
+	},
+	plugins: [],
+}
